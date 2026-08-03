e06122f4caab2c1915bdbf7c8eb58bc5a8866921
Sk Tayus Uddin <sktayusuddin@gmail.com>
2026-08-03 15:43:17 +0530

    Time: 0 ms (100%), Space: 44.8 MB (6.01%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -1,20 +1,10 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
-        int m = accounts.length;
-        int n = accounts[0].length;
-
-        int[] ans = new int[m];
-        int sum = 0;
-        for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
-                sum+= accounts[i][j];
-            }
-            ans[i] = sum;
-            sum = 0;
-        }
         int max = 0;
-        for(int x : ans) {
-            if(x > max) max = x;
+        for (int[] row : accounts) {
+            int sum = 0;
+            for (int x : row) sum += x;
+            max = Math.max(max, sum);
         }
         return max;
     }
