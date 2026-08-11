4297f33002b1291a84976f71d87464f6e01d9f1b
Sk Tayus Uddin <sktayusuddin@gmail.com>
2026-08-11 11:50:57 +0530

    Time: 2 ms (23.09%), Space: 47.2 MB (82.93%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        int estimatedSum = (n)*(n+1)/2;
+        System.out.println(estimatedSum);
+
+        for(int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+        return estimatedSum - sum;
+    }
+}
