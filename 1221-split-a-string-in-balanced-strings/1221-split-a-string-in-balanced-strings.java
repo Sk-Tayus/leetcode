79db5fbc0e8ba2cbc0af8330cbd4e088fe5a8ae8
Sk Tayus Uddin <sktayusuddin@gmail.com>
2026-08-20 15:19:39 +0530

    Time: 0 ms (100%), Space: 43 MB (9.27%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int count = 0;
+        int Rcount = 0, Lcount = 0;
+
+        int n  = s.length();
+        int i=0;
+        while(i<n) {
+            char ch = s.charAt(i);
+            if(ch == 'R') Rcount++;
+            // while(ch != 'R') {
+            //     Rcount++;
+            //     i++;
+            // }
+            if(ch == 'L') Lcount++;
+            // while(ch != 'L') {
+            //     Lcount++;
+            //     i++;
+            // }
+            if(Rcount == Lcount) {
+                count++;
+                Rcount=0;
+                Lcount=0;
+            }
+            i++;
+
+        }
+        return count;
+    }
+}
