81f88760dbd335487bca8d8677bbcc0b6cdd84a0
Sk Tayus Uddin <sktayusuddin@gmail.com>
2026-08-27 23:16:08 +0530

    Time: 0 ms (100%), Space: 44.4 MB (96.53%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public int searchInsert(int[] nums, int target) {
+         int n = nums.length;
+        int st = 0, end = n - 1;
+        int idx = -1;
+
+        while (st <= end) {
+            int mid = (st + end) / 2;
+            if (nums[mid] == target) {
+                idx = mid;
+                return idx;
+            }
+            else if (target < nums[mid]) {
+                end = mid - 1;
+            }
+
+            else if (target > nums[mid]) {
+                st = mid+1;
+            }
+        }
+        if(idx == -1) return end+1;
+        return idx;
+    }
+}
