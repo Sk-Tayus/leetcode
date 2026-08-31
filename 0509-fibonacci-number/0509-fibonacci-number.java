30ae09ed324b5e1720ca9945c6f5d106c046daa0
Sk Tayus Uddin <sktayusuddin@gmail.com>
2026-08-31 20:38:07 +0530

    Time: 0 ms (100%), Space: 42 MB (70.39%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,8 +1,15 @@
+// 0 1 1 2 3 5 8 13 
 class Solution {
     public int fib(int n) {
-        if(n == 0) return 0;
-        if(n == 1) return 1;
+        int a=0, b=1;
+        int c = a+b;
 
-        return fib(n-1) + fib(n-2);
+        for(int i=0; i< n; i++) {
+            a = b;
+            b = c;
+            c = a+b;
+        }
+
+        return a;
     }
 }
