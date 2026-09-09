50d4789904f2c6d6ee391edafd9b48af11e60a66
Sk Tayus Uddin <sktayusuddin@gmail.com>
2026-09-09 15:30:15 +0530

    Time: 6 ms (7.45%), Space: 46.9 MB (14%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,41 +1,31 @@
-import java.util.HashMap;
-
 class Solution {
     public int romanToInt(String s) {
-        // Step 1: Create the Roman numeral value map
-        HashMap<Character, Integer> map = new HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
+        Map<Character, Integer> map = Map.of(
+            'I', 1,
+            'V', 5,
+            'X', 10,
+            'L', 50,
+            'C', 100,
+            'D', 500,
+            'M', 1000
+        );
 
-        int sum = 0;
-        int i = 0;
 
-        while (i < s.length()) {
-            char currChar = s.charAt(i);
-            int currVal = map.get(currChar);
+        int n = s.length();
+        int total = 0;
 
-            // Check for next character
-            if (i + 1 < s.length()) {
-                char nextChar = s.charAt(i + 1);
-                int nextVal = map.get(nextChar);
+        for(int i=0;i<n-1;i++) {
+            char ch = s.charAt(i);
+            char ch2 = s.charAt(i+1);
 
-                if (currVal < nextVal) {
-                    sum += (nextVal - currVal);
-                    i += 2; // Skip the next char (used already)
-                    continue;
-                }
+            if(map.get(ch) < map.get(ch2)) {
+                total -= (map.get(ch));
             }
-
-            // Regular case
-            sum += currVal;
-            i++;
+            else if(map.get(ch) >= map.get(ch2)) {
+                total += map.get(ch);
+            }
         }
-
-        return sum;
+        total += map.get(s.charAt(n-1));
+        return total;
     }
 }
