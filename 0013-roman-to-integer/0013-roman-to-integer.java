d878fb2738f0cdd5a0e69621be2a6c47925f74c9
Sk Tayus Uddin <sktayusuddin@gmail.com>
2026-09-09 15:32:54 +0530

    Time: 2 ms (100%), Space: 44.8 MB (99.96%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,31 +1,30 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<Character, Integer> map = Map.of(
-            'I', 1,
-            'V', 5,
-            'X', 10,
-            'L', 50,
-            'C', 100,
-            'D', 500,
-            'M', 1000
-        );
+        int[] map = new int[26]; // A-Z → 0 to 25
+        map['I' - 'A'] = 1;
+        map['V' - 'A'] = 5;
+        map['X' - 'A'] = 10;
+        map['L' - 'A'] = 50;
+        map['C' - 'A'] = 100;
+        map['D' - 'A'] = 500;
+        map['M' - 'A'] = 1000;
 
+        int sum = 0;
+        int len = s.length();
 
-        int n = s.length();
-        int total = 0;
-
-        for(int i=0;i<n-1;i++) {
-            char ch = s.charAt(i);
-            char ch2 = s.charAt(i+1);
-
-            if(map.get(ch) < map.get(ch2)) {
-                total -= (map.get(ch));
-            }
-            else if(map.get(ch) >= map.get(ch2)) {
-                total += map.get(ch);
+        for (int i = 0; i < len - 1; i++) {
+            int curr = map[s.charAt(i) - 'A'];
+            int next = map[s.charAt(i + 1) - 'A'];
+            if (curr < next) {
+                sum -= curr;
+            } else {
+                sum += curr;
             }
         }
-        total += map.get(s.charAt(n-1));
-        return total;
+
+        // Add last character (always gets added)
+        sum += map[s.charAt(len - 1) - 'A'];
+
+        return sum;
     }
 }
